f133addbd202d3f063c40f63d4fe069d57b5714b
sricharanreddy11 <sricharanteegala@gmail.com>
2024-03-18 12:57:19 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
+const downloadBtn = document.getElementById("download-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+tabBtn.addEventListener("click", () => {    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+
+deleteBtn.addEventListener("dblclick", () => {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+downloadBtn.addEventListener("click",()=>{
+    var url = 'data:application/json;base64,' + btoa(leadsFromLocalStorage);
+    chrome.downloads.download({
+        url: url,
+        filename: 'leads.json'
+    });
+})
+
+inputBtn.addEventListener("click", function (){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
